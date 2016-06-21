d3434ae848ccc859bfcd23e0101ddb56801b0108
Alex <alex@alex-VirtualBox.(none)>
2016-06-21 20:30:07 +0300

    event-mailer, sendgrid

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,8 @@
+# Базовый класс для всех мэйлеров приложения
+class ApplicationMailer < ActionMailer::Base
+  # обратный адрес всех писем по умолчанию
+  default from: "alezibarev.gmail.com"
+
+  # Задаем макет для всех писем
+  layout 'mailer'
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors =true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -39,5 +39,18 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # Базовый урл сайта, для генерации правильных ссылок в письмах
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-end
+
+  # отправка почты по протоколу SMTP
+  config.action_mailer.delivery_method = :smtp
+
+  # Настройки для работы через GMail аккаунт
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: '587',
+      user_name: 'alezibarev', # не используйте для тестов свои реальные ящики
+      password: '', # не храните здесь пароль!
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }end
