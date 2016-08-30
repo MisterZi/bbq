6b5da00f9def6f50eb3305f3f5a270576c88d420
Alex <alex@alex-VirtualBox.(none)>
2016-08-30 20:33:33 +0300

    small fix

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -21,7 +21,7 @@ class SubscriptionsController < ApplicationController
 
     if @new_subscription.save
       # Отправляем письмо автору события
-      # EventMailer.subscription(@event, @new_subscription).deliver_now
+      EventMailer.subscription(@event, @new_subscription).deliver_now
       # если сохранилась успешно, редирект на страницу самого события
       redirect_to @event, notice: I18n.t('controllers.subscription.created')
     else
